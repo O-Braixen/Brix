44d9f804b1157d53b8a136cc3a006d2addbef25b
Vinicius Sena da Silva Amarante <viniciussena.amarante@gmail.com>
2025-08-21 22:23:41 -0300

    ajuste arquivo de exemplo.env

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Client(commands.AutoShardedBot):
         intents.message_content = True
         intents.members = True
 
-        super().__init__(command_prefix='-', intents=intents, shard_count=2)
+        super().__init__(command_prefix='-', intents=intents, shard_count=3)
         self.synced = False  # Isso é usado para que o bot não sincronize os comandos mais de uma vez
         self.cogslist = []
         for cog in listdir("src/services/modules"):
